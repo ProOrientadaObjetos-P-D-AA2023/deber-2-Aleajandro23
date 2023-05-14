b91461a9ba29151368644d6d025ef14e2e047c48
Ruben Condoy <rubenaleajandro@gmail.com>
2023-05-14 01:23:38 -0500

    TAreaa

--- ejemplos/mis_clases.py
+++ ejemplos/mis_clases.py
@@ -8,6 +8,23 @@
 
 # clase 01
 
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print("Hola, mi nombre es", self.nombre)
+
+# Crear una instancia de la clase Persona
+persona1 = Persona("Ruben", 25)
+
+# Acceder a los atributos de la instancia
+print(persona1.nombre)  # salida: Ruben
+print(persona1.edad)    # salida: 25
+
+# Llamar al método de la instancia
+persona1.saludar()      # salida: Hola, mi nombre es Ruben
 
 
 # clase 02
